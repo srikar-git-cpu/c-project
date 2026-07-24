5eb676355ed786e8a00e85e08d0e9f3fa924dd78
Nagubandi srikar <nagubandisrikar73@gmail.com>
2026-07-24 18:03:02 +0530

    Add project-2.cpp file

---
+++ project-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    double sales = 200000;
+    cout << "sales: $" << sales << endl;
+    double stateTax = sales * 0.04;
+    cout << "state tax: $" << stateTax << endl;
+    return 0;
+}
